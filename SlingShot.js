c2e68ed5fe135f288f6e87a9497fd5e91972df06
Anandita-coding <71819248+Anandita-coding@users.noreply.github.com>
2020-11-27 09:04:48 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,32 @@
+class Slingshot{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 0.04,
+            length: 2
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    console.log(this.sling.bodyA.position)
+    }
+    attach(body){
+        this.sling.bodyA = body
+        }
+
+    fly(){
+this.sling.bodyA = null
+    }
+    display(){                                              
+       if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    } 
+    }
+    
+
+
+
+}
